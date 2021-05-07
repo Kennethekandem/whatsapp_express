8d808c342a77e95fea3e69db02f1d22d39d08c5a
Kenneth Ekandem <kennyekandem@gmail.com>
2021-05-07 15:59:19 +0100

    update to index, added body parser and multer for form

--- index.ts
+++ index.ts
@@ -5,8 +5,20 @@ const app = express();
 
 require('dotenv').config();
 require('./connectors/mongodb');
-let route : any = require('./routes');
+let bodyParser = require('body-parser');
+let route = require('./routes');
+let multer = require('multer');
 
+/*
+    Body parser for get items from form body
+*/
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// multer for getting files from form
+app.use(multer().array())
+
+// Entry route for APIs
 app.use('/', route);
 
 const port = process.env.PORT;
