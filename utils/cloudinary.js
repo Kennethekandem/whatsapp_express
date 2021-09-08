6946645ca55cc625382c1d438756c61dc539f8c0
Kenneth Ekandem <kennyekandem@gmail.com>
2021-09-08 18:08:39 +0100

    testing browser text editor

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,6 +1,7 @@
 const cloudinary = require("cloudinary");
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    //this is testing if that works just fine
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
